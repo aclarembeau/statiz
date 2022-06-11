dddc3370a1375b42314817b43d4b9a4e31c4a1ec
Alexis Clarembeau <alexis.clarembeau@gmail.com>
2022-06-11 12:41:17 +0200

    fix new

--- src/actions/new.js
+++ src/actions/new.js
@@ -1,5 +1,5 @@
 let fse = require("fs-extra");
 let path = require("path");
 module.exports = (directory) => {
-  fse.copySync(path.join(__dirname, "sample/"), directory);
+  fse.copySync(path.join(path.basename(__dirname), "sample/"), directory);
 };
