9acb7f7b81f094c98b3ca0cee170d610f856f96a
Alexis Clarembeau <alexis.clarembeau@gmail.com>
2022-06-11 12:46:52 +0200

    fix new

--- src/actions/new.js
+++ src/actions/new.js
@@ -1,5 +1,13 @@
 let fse = require("fs-extra");
+let fs = require("fs");
 let path = require("path");
 module.exports = (directory) => {
-  fse.copySync(path.join(path.resolve(__dirname, "../"), "sample"), directory);
+  if (fs.existsSync(directory)) {
+    console.error("Cannot start on a existing directory: ", directory);
+    return;
+  }
+  fse.copySync(
+    path.join(path.resolve(__dirname, "../../"), "sample"),
+    directory
+  );
 };
